b458143f64b812c6a55743d11c0830ec77904528
forbearing <fredjoseph863@gmail.com>
2021-09-07 20:51:38 +0800

    fish config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,13 @@ set -x PATH /opt/local/bin /opt/local/sbin $PATH
 
 if test -f /Users/hybfkuf/.autojump/share/autojump/autojump.fish; . /Users/hybfkuf/.autojump/share/autojump/autojump.fish; end
 if test -f /usr/local/share/autojump/autojump.fish; . /usr/local/share/autojump/autojump.fish; end
+
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
+
+set -gx PATH $PATH $HOME/.krew/bin
